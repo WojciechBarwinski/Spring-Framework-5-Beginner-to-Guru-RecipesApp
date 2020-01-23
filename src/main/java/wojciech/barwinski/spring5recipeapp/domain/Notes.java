96dee59a657ca6126963947a7b0fa95aee2834dc
Wojciech Barwiński <wojciech.barwinski@gmail.com>
2020-01-23 20:17:27 +0100

    Create 4 entity

---
+++ src/main/java/wojciech/barwinski/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,37 @@
+package wojciech.barwinski.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
