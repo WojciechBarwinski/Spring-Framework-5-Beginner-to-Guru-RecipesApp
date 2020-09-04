f741dbda34f6cc4a82afaf04af98a6714acf1033
thaaz <thaazar@gmail.com>
2020-09-04 15:52:18 +0200

    s10/ odc 178

---
+++ src/test/java/wojciech/barwinski/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,30 @@
+package wojciech.barwinski.spring5recipeapp.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setUP(){
+        category = new Category();
+    }
+    @Test
+    void getId() {
+        Long idValue = 4l;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
---
+++ src/test/java/wojciech/barwinski/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,41 @@
+package wojciech.barwinski.spring5recipeapp.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import wojciech.barwinski.spring5recipeapp.domain.Recipe;
+import wojciech.barwinski.spring5recipeapp.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
